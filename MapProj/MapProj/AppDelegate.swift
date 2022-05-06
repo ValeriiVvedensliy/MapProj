e4a1681de8fb480ba0a6429470c46aef286f3ecc
Valerii <valeronmaestro@gmail.com>
2022-05-06 21:18:30 +0300

    commit

--- MapProj/MapProj/AppDelegate.swift
+++ MapProj/MapProj/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
-    GMSServices.provideAPIKey("AIzaSyC6_iYti5Jj8-0qRT1FyEL-IfS3FFgCzm4")
     return true
   }
 
